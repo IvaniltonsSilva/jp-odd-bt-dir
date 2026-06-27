529a667ba83b2bf5350a1b7fd56dad69c19fd625
JCPrime01 <vanzokiller@gmail.com>
2026-06-27 12:37:34 -0300

    chore: shareCode BFDD2P6T9S3 -> 4DTIBNSY215 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=BFDD2P6T9S3&afp1=oa&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=4DTIBNSY215&afp1=oa&home=1' });
     res.end();
   }
