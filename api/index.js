437c313ecbd4e80b7f1a0465f5ef42fb9975431c
JCPrime01 <vanzokiller@gmail.com>
2026-07-03 15:51:10 -0300

    chore: shareCode U6US8R3B935 -> MYC1NNZ9CFF (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=U6US8R3B935&afp1=oa&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=MYC1NNZ9CFF&afp1=oa&home=1' });
     res.end();
   }
