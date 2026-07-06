72967a5bb02d9d55ecaf93302b1dc9360c18e9a4
JCPrime01 <vanzokiller@gmail.com>
2026-07-06 10:50:46 -0300

    chore: shareCode MYC1NNZ9CFF -> UAUDZJTQPZ9 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=MYC1NNZ9CFF&afp1=oa&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=UAUDZJTQPZ9&afp1=oa&home=1' });
     res.end();
   }
